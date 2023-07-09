eb789f9ea82e83d68f5302b5fa780dae4fc49ba5
Alexandr Toropov <toropovsite@yandex.ru>
2023-07-09 16:07:35 +0300

    Тесты для функции определения пола человека по его имени

--- 2/tests.php
+++ 2/tests.php
@@ -6,3 +6,8 @@
 
 assert(-7 === getDiscriminant(2 , 1, 1));
 assert(-39 == getDiscriminant(6, -3, 2));
+
+assert('male' === checkGender('Саша'));
+assert('male' === checkGender('Дима'));
+assert('female' === checkGender('Настя'));
+assert('female' === checkGender('Маша'));
