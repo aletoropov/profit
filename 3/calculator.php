003599ab9692c0638518b54303f5579deb38def6
Alexandr Toropov <toropovsite@yandex.ru>
2023-07-11 15:31:17 +0300

    Задание №1 - Калькулятор

---
+++ 3/calculator.php
@@ -0,0 +1,64 @@
+<?php
+
+/**
+ * Напишите программу-калькулятор
+ * Форма для ввода двух чисел, выбора знака операции и кнопка "равно"
+ * Данные пусть передаются методом GET из формы (да, можно и так!) на скрипт,
+ * который их примет и выведет выражение и его результат
+ * Попробуйте улучшить программу. Пусть данные отправляются на ту же страницу на PHP,
+ * введенные числа останутся в input-ах, а результат появится после кнопки "равно"
+ */
+
+
+?>
+
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Калькулятор</title>
+</head>
+<body>
+
+<?php
+
+if (!empty($_GET)) {
+    $operand1 = filter_input(INPUT_GET, 'operand1');
+    $operand2 = filter_input(INPUT_GET, 'operand2');
+    $action = filter_input(INPUT_GET, 'action');
+
+    switch ($action) {
+        case '+':
+            echo $operand1 . ' + ' . $operand2 . ' = ' . $operand1 + $operand2;
+            break;
+        case '-':
+            echo $operand1 . ' - ' . $operand2 . ' = ' . $operand1 - $operand2;
+            break;
+        case '*':
+            echo $operand2 . ' * ' . $operand2 . ' = ' . $operand1 * $operand2;
+            break;
+        case '/':
+            echo $operand2 . ' / ' . $operand2 . ' = ' . $operand1 / $operand2;
+            break;
+        default:
+            echo 'Не удалось найти ответ';
+    }
+}
+
+?>
+    <form name="calculator" method="get">
+        <input type="number" name="operand1">
+        <select name="action">
+            <option value="+">+</option>
+            <option value="-">-</option>
+            <option value="*">*</option>
+            <option value="/">/</option>
+        </select>
+        <input type="number" name="operand2">
+        <input type="submit" value="=">
+    </form>
+</body>
+</html>
