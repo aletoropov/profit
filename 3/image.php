30d3f51368ccb666a37213cf12bf1d0e8d4ca31e
Alexandr <toropovsite@yandex.ru>
2023-12-02 06:04:59 +0300

    Вывод подписи при показе изображения

--- 3/image.php
+++ 3/image.php
@@ -4,7 +4,7 @@ $imageId = (int) filter_input(INPUT_GET, 'id');
 $imageList = include 'images.php';
 
 if (array_key_exists($imageId, $imageList)) {
-    echo '<img src="' . $imageList[$imageId] . '">';
+    echo '<div><img src="' . $imageList[$imageId] . '"><p>Изображение: '. $imageList[$imageId] . '</p></div>';
 } else {
     echo 'Изображение не найдено!';
 }
