ce51a112caef9b528c4a8a288ccf7549ed0f6da0
Alexandr <toropovsite@yandex.ru>
2023-12-02 15:03:15 +0300

    Изменил пути до файлов с изображениями

--- 3/images.php
+++ 3/images.php
@@ -5,8 +5,8 @@
  */
 
 return [
-    1 => __DIR__ . 'images' . DIRECTORY_SEPARATOR . 'img1.jpg',
-    2 => __DIR__ . 'images' . DIRECTORY_SEPARATOR . 'img2.jpg',
-    3 => __DIR__ . 'images' . DIRECTORY_SEPARATOR . 'img3.jpg',
-    4 => __DIR__ . 'images' . DIRECTORY_SEPARATOR . 'img4.jpg',
+    1 => 'images' . DIRECTORY_SEPARATOR . 'img1.jpg',
+    2 => 'images' . DIRECTORY_SEPARATOR . 'img2.jpg',
+    3 => 'images' . DIRECTORY_SEPARATOR . 'img3.jpg',
+    4 => 'images' . DIRECTORY_SEPARATOR . 'img4.jpg',
 ];
