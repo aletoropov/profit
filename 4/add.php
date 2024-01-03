84380b7d6a5a79176473c4d7bf30ceaab43bf015
Alexandr <toropovsite@yandex.ru>
2024-01-03 18:17:12 +0300

    Контроллер добавления новой записи в гостевую книгу

--- 4/add.php
+++ 4/add.php
@@ -1,1 +1,8 @@
 <?php
+
+require __DIR__ . DIRECTORY_SEPARATOR . 'functions.php';
+
+$message = filter_input(INPUT_POST, 'message');
+addGbMessage($message, 'db.txt');
+header('Location: guestbook.php');
+exit();
