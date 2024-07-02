ffee7629ef03ab13d7bcd6e24fc9b7bac683defd
Александр Торопов <toropovsite@yandex.ru>
2024-07-03 01:19:03 +0300

    Загрузка изображения

---
+++ 4/upload.php
@@ -0,0 +1,25 @@
+<?php
+
+require __DIR__ . DIRECTORY_SEPARATOR . 'functions.php';
+
+$path = __DIR__ . DIRECTORY_SEPARATOR . 'images' . DIRECTORY_SEPARATOR;
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    if (isset($_FILES['image'])) {
+        if (!is_dir($path)) {
+            mkdir($path, 0777, true);
+        }
+
+        $uploadedFile     = $_FILES['image']['tmp_name'];
+        $uploadedFileName = $_FILES['image']['name'];
+        $errorCode        = $_FILES['image']['error'];
+    }
+
+    if ($errorCode == 0) {
+        if (move_uploaded_file($uploadedFile, $path . $uploadedFileName)) {
+            echo 'Файл загружен!';
+        } else {
+            echo $errorCode;
+        }
+    }
+}
